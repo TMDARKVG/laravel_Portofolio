a5b252ee99195ae8a17fbeb2d14f9e2cf413fc9c
TMDARKVG <115409644+TMDARKVG@users.noreply.github.com>
2023-09-28 19:01:36 +0700

    tes aja dulu

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/resume', function () {
+    return view('resume');
+});
+Route::get('/projects', function () {
+    return view('projects');
+});
+Route::get('/', function () {
+    return view('index');
+});
